2a63210cd40116eee368ff8f470be47e07728748
nontakorn168 <158499054+nontakorn168@users.noreply.github.com>
2024-05-27 14:56:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+// index.js (or wherever you define your functions)
+
+// Function Declaration: saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`; 
+  }
+  
+  // Function Expression: mondayWork
+  const mondayWork = function(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+  };
+  
+  // Curried Function: wrapAdjective
+  function wrapAdjective(wrapper) {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`; 
+    };
+  }
+     
+  const expect = require("chai").expect; // Assuming you have Chai installed
+  
+  describe("index.js", () => {
+    // ... (tests for saturdayFun, mondayWork, and wrapAdjective as you provided)
+  }); 
+  
